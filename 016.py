dbdc88f09ace83da176d849d9af47098d1d8f7be
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 21:45:08 +0530

    Update 016.py

--- 016.py
+++ 016.py
@@ -1,43 +1,21 @@
-def prime(n):
-
-
-l=[]
-
-for k in range(2,n):
-
-if k==2:
-
-l.append(2)
-
+def primeless(nn):
+    ll=[]
+    for k in range(2,nn):
+        if k==2:
+            ll.append(2)
+        else:
+            for i in range(2,k):
+                if k%i==0:
+                    break
+            if i==k-1:
+                ll.append(k)
+    return l
+nn=int(input())
+ll=primeless(nn)
+if(len(ll)>0):
+    if(ll[-1] == 97):
+        print(" ".join(map(str,ll)),"")
+    else:
+        print(" ".join(map(str,ll)))
 else:
-
-for i in range(2,k):
-
-if k%i==0:
-
-break
-
-if i==k-1:
-
-l.append(k)
-
-return l
-
-n=int(input())
-
-l=prime(n)
-
-if(len(l)>0):
-
-if(l[-1] == 97):
-
-print(" ".join(map(str,l)),"")
-
-else:
-        print(" ".join(map(str,l)))
-
-else:
-
-print(0)
-
-
+    print(0)
