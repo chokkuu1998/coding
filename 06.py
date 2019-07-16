e0c53334e6667b2a53f6153ec7385842c10309da
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 21:12:17 +0530

    Update 06.py

--- 06.py
+++ 06.py
@@ -5,14 +5,13 @@ ar=list(map(int,input().split()))[:x]
 ct=0
 
 for i in range(0,x-2):
- 
+
  for j in range(i+1,x-1):
  
  for k in range(i+2,x):
  
  if(ar[i]+ar[j])==ar[k] :
  
+ ct+=1           
  
- ct+=1 
- 
-print(ct)
+ print(ct)
