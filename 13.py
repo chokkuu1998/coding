fd768d9c832ad5a29e3c7d29813e202aa424719d
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 19:40:55 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,8 +1,7 @@
-nn=int(input())
-l=[]
-for i in range(nn):
-	l1=[int(x) for x in input().split()]
-	mm=sum(l1)
-	l.append(mm)
-mm=sum(l)/(nn*nn)
-print("%.6f" %mm)
+from itertools import chain
+from statistics import mean
+nnn = int(input())
+rrr = []
+for _ in range(nnn):
+    rrr.append(list(map(int, input().split())))
+print(format((mean(chain.from_iterable(rrr))), '.6f'))
