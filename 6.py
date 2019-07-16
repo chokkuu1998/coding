c5b6df18bebcd86776fa0a8d2856c3294943b365
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 14:19:34 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+ss=input()
+ss=ss.replace(".","")
+l=ss.split()
+k=""
+for i in range(1,len(l)+1):
+    if i%2!=0:
+        k=k+l[i-1][::-1]+" "
+    else:
+        k=k+l[i-1]+" "
+print(k.strip())        
+#rev_at_odd
