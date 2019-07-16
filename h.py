238d6fd7a0ca58cf0f9aed1168536eb6500be972
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 14:01:43 +0530

    Update h.py

--- h.py
+++ h.py
@@ -1,10 +1,11 @@
-aa=input()
-tt=0
-ll=list(combinations(a,len(aa)-1))
+from itertools import combinations
+pp=input()
+qq=0
+ll=list(combinations(pp,len(pp)-1))
 for i in range(len(l)):
-    if l[i]==l[i][::-1]:
-        print("YES")
-        t=1
-        break
-if t==0:
+         if(l[i]==l[i][ ::-1]):
+             print("YES")
+             qq=1
+             break
+if(qq==0):
     print("NO")
