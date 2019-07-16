cefb74b77710e6d3c6c4dbd21dce8a3817c08862
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 21:39:40 +0530

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,26 +1,13 @@
-N=input()
-
-
-temp=""
-
-f=0
-
-for i in range(len(N)):
-
-if N[i]==" ":
-
-temp+=N[i]
-
-elif f==0:
-
-temp+=N[i].upper()
-
-f=1
-
-elif f==1:
-
-temp+=N[i]
-
-f=0
-
-print(temp)
+tt=input()
+ss=""
+nn=0
+for i in range(len(tt)):
+    if tt[i]==" ":
+        ss=ss+tt[i]
+    elif  nn%2!=0:
+        ss=ss+tt[i]
+        nn=nn+1 
+    else:
+        ss=ss+tt[i].upper()
+        nn=nn+1
+print(ss)
