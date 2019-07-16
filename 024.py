5a98a204926cc073791ebd88c74197840d6e6c13
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 22:04:23 +0530

    Update 024.py

--- 024.py
+++ 024.py
@@ -1,20 +1,46 @@
-NN=int(input())
-aa=[]
-cc=0
-for i in range(2,NN):
+n=int(input())
+
+
+l=[]
+
+count=0
+
+for i in range(2,n):
+
     for j in range(2,i+1):
-        if i%j==0:
-            break
+	
+    if i%j==0:
+	
+    break
+	
     if j==i:
-        aa.append(i)
-for i in range(len(aa)):
-    for j in range(len(aa)):
-        for k in range(len(aa)):
-            if aa[i]+aa[j]+aa[k]==N:
-                print(str(aa[i])+" "+str(aa[j])+" "+str(aa[k]))
-                cc=1
-                break
-        if cc==1:
-            break
-    if cc==1:
-        break
+	
+    l.append(i)
+for 
+i in range(len(l)):
+
+    for j in range(len(l)):
+	
+    for k in range(len(l)):
+	
+    if l[i]+l[j]+l[k]==n:
+	
+    print(str(l[i])+" "+str(l[j])+" "+str(l[k]))
+	
+    count=1
+	
+    
+    break
+	
+    if count==1:
+	
+    break
+	
+    if count==1:
+	
+    break
+    
+    
+    
+    
+        
